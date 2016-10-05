654ecb2ded01ea45aac11821d4b9ea81d6425a4b
pshriwise <shriwise@wisc.edu>
2016-10-04 23:46:52 -0500

    Passing vertex data to OSPRay from a MOAB instance.

--- apps/qtViewer/sg/importer/ImportMOAB.cpp
+++ apps/qtViewer/sg/importer/ImportMOAB.cpp
@@ -16,6 +16,7 @@
 // moab
 #include "moab/Core.hpp"
 #include "moab/Interface.hpp"
+#include "moab/CartVect.hpp"
 
 namespace ospray {
   namespace sg {
@@ -66,7 +67,55 @@ namespace ospray {
 
     Ref<sg::TriangleMesh> MOABLoader::readFile(std::string fn) {
       moab::Interface *mbi = new moab::Core();
-      Ref<sg::TriangleMesh> m;
+      moab::ErrorCode rval;
+      
+      rval = mbi->load_mesh(fn.c_str());
+      MB_CHK_SET_ERR_CONT(rval, "Could not open model file.");
+      
+      Ref<sg::TriangleMesh> m = new sg::TriangleMesh;
+      Ref<sg::DataVector3f> pos = new sg::DataVector3f;
+      Ref<sg::DataVector3f> nor = new sg::DataVector3f;
+      Ref<sg::DataVector3i> idx = new sg::DataVector3i;
+
+      std::vector<moab::EntityHandle> tris, verts;
+      rval = mbi->get_entities_by_type(0, moab::MBTRI, tris, true);
+      MB_CHK_SET_ERR_CONT(rval, "Could not get the model triangles.");
+
+      rval = mbi->get_entities_by_type(0, moab::MBVERTEX, verts, true);
+      MB_CHK_SET_ERR_CONT(rval, "Could not get the model vertices.");
+
+      std::cout << "Number of triangles in the model: " << tris.size() << std::endl;
+      std::cout << "Number of vertices in the model: " << verts.size() << std::endl;
+      pos->v.resize(verts.size());
+      // nor->v.resize(verts.size()); only if we want to handle vertex normals
+      std::map<moab::EntityHandle, int> index_map;
+      for( unsigned int i = 0; i < verts.size(); i++){
+	moab::EntityHandle vert = verts[i];
+	moab::CartVect coords;
+	rval = mbi->get_coords( &vert, 1, coords.array() );
+	MB_CHK_SET_ERR_CONT(rval, "Could not get vertex coordinates");
+	pos->v[i] = vec3f(coords[0],coords[1],coords[2]);
+	index_map[vert] = i;
+      }
+
+      for(unsigned int i = 0; i < tris.size(); i++) {
+	moab::EntityHandle tri = tris[i];
+	moab::EntityHandle const *conn = NULL;
+	int len;
+	std::vector<moab::EntityHandle> dum_vector;
+	rval = mbi->get_connectivity(tri, conn, len, true, &dum_vector);
+	MB_CHK_SET_ERR_CONT(rval, "Could not get triangle connectivity");
+
+	vec3i vtx;
+	vtx.x = index_map[conn[0]];
+	vtx.y = index_map[conn[1]];
+	vtx.z = index_map[conn[2]];
+
+	idx->v.push_back(vtx);
+      }
+
+      m->index = idx.ptr;
+      m->vertex = pos.ptr;
       return m;
     };
     
@@ -75,6 +124,7 @@ namespace ospray {
       std::cout << "ospray::sg::importMOAB: importing from " << fileName << endl;
       MOABLoader loader(world.ptr,fileName);
       Ref<sg::TriangleMesh> mesh = loader.readFile(fileName.str());
+      world->node.push_back(mesh.cast<Node>());
     }
 
 
